8b83bb9c2500514b8965c22932fb5fa6684e38a5
Gagan Sharma <52528493+gagansh7171@users.noreply.github.com>
2020-03-24 16:23:58 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "localhost";
-$user = "test" ;
-$pass = "helloiitr";
-$db = "phpassign";
+$host = "ec2-54-164-211-9.compute-1.amazonaws.com";
+$user = "first_year" ;
+$pass = "first_year";
+$db = "first_year";
 $mysqli = new mysqli($host, $user, $pass,$db) or die($mysqli->error);
 
 
